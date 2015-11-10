8e9fd3b019407996d642b65f863b5b54cf7d6fad
HIRAM MADELAINE <hiram.madelaine@gmail.com>
2015-11-10 09:18:13 +0100

    bump om-next version

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "My first Om program!"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.170"]
-                 [org.omcljs/om "1.0.0-alpha16-SNAPSHOT"]
+                 [org.omcljs/om "1.0.0-alpha20-SNAPSHOT"]
                  [datascript "0.13.2"]
                  [figwheel-sidecar "0.5.0-SNAPSHOT" :scope "provided"]]
 
